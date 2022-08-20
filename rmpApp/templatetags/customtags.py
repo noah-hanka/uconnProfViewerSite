b9ba3791944c58547dc1fff542402b3bcff60cad
Noah <hankanz@outlook.com>
2022-08-19 23:57:01 -0400

    added rating coloring and styling

--- rmpApp/templatetags/customtags.py
+++ rmpApp/templatetags/customtags.py
@@ -1,3 +1,4 @@
+from contextlib import redirect_stderr
 from django import template
 
 register = template.Library()
@@ -7,4 +8,13 @@ def dic(dic,key):
     return dic[key]
 @register.filter
 def na(value):
-    return "N/A" if value == -1 else value
+    return "N/A" if value == -1 else str(value)+'%'
+@register.filter
+def ratingcolor(value):
+    value = int(value)
+    color = '#FF6961'
+    if value >= 2.5:
+        color = '#F8D66D'
+    if value >= 4:
+        color = '#8CD47E'
+    return color
