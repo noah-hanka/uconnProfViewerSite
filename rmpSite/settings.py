2fd3869c0cefa6126a34a16d19ad97b062f68c20
Noah <hankanz@outlook.com>
2022-09-05 18:29:33 -0400

    updated settings

--- rmpSite/settings.py
+++ rmpSite/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path_dirname(os.path.dirname(os.path.abspath(__file__))))
+STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(BASE_DIR), 'static'))
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
